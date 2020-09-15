bd93d654a725639edd520cf18c0edea04aa81019
Sujeet Kumar <sujitprajapati557@gmail.com>
2020-09-15 21:35:13 +0530

    Welcome controller

---
+++ DemoApplication/src/main/java/com/sktech/SKTechApplication/SKTechApplication.java
@@ -0,0 +1,13 @@
+package com.sktech.SKTechApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SKTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SKTechApplication.class, args);
+	}
+
+}
