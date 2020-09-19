a387d6eb0252771ba4a8195a403d58d2de72e25f
Sujeet Kumar <sujitprajapati557@gmail.com>
2020-09-19 10:11:36 +0530

    Spring Boot Crud Operation of Book Details with Spring Data JPA with MySQL and Swagger2

--- SKTechCRUDApi/src/main/java/com/sktech/SKTechCRUDApi/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.sktech.SKTechCRUDApi;
--- SKTechCRUDApi/src/main/java/com/sktech/SKTechCRUDApi/repository/BooksRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sktech.SKTechCRUDApi.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.sktech.SKTechCRUDApi.model.Books;
-//repository that extends CrudRepository
-public interface BooksRepository extends CrudRepository<Books, Integer>
-{
-}
